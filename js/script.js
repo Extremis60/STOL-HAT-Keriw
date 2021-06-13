1ff8d540f8934a6ebe6631a12503088ca942e836
Extremis60 <kacper.mazur04@protonmail.com>
2021-06-13 13:21:22 +0200

    a

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Scroll Up Button skrypt
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if($(this).scrollTop()>40){
+			$('#scrollUpBtn').fadeIn();
+		}else{
+			$('#scrollUpBtn').fadeOut();
+		}	
+	});
+
+
+	$("#scrollUpBtn").click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+	});
+});
+
+/*slide test*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides() {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	for (i = 0; i < slides.length; i++) {
+	  slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {slideIndex = 1}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlides, 3000);
+  }
